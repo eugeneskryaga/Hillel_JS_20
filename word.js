e5fe9b77ffba5483a33e72a0327724127b01d216
Eugene <zhenyaskryaga@gmail.com>
2025-12-03 00:14:11 +0200

    work of mistakes

--- word.js
+++ word.js
@@ -1,18 +1,18 @@
-// let word = prompt("Enter one word: ");
+let word = prompt("Enter one word: ");
 
-// if (word === null) {
-//   alert("Canceled.");
-// } else if (word.trim().includes(" ") || word.trim() === "") {
-//   alert("Invalid data input.");
-// } else {
-//   word = word.trim();
-//   if (word.length % 2 === 0) {
-//     alert(
-//       `Word: ${word}, result: ${word[word.length / 2 - 1]}${
-//         word[word.length / 2]
-//       }`
-//     );
-//   } else {
-//     alert(`Word: ${word}, result: ${word[Math.floor(word.length / 2)]}`);
-//   }
-// }
+if (word === null) {
+  alert("Canceled.");
+} else if (word.trim().includes(" ") || word.trim() === "") {
+  alert("Invalid data input.");
+} else {
+  word = word.trim();
+  if (word.length % 2 === 0) {
+    alert(
+      `Word: ${word}, result: ${word[word.length / 2 - 1]}${
+        word[word.length / 2]
+      }`
+    );
+  } else {
+    alert(`Word: ${word}, result: ${word[Math.floor(word.length / 2)]}`);
+  }
+}
