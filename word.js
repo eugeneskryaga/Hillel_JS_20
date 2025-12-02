1b7db252baf49e8d4107b8c56cfe9ca265f99f0b
Eugene <zhenyaskryaga@gmail.com>
2025-12-03 00:13:49 +0200

    work of mistakes

--- word.js
+++ word.js
@@ -1,20 +1,18 @@
-let isInputValid = false;
-let word;
+// let word = prompt("Enter one word: ");
 
-while (isInputValid === false) {
-    word = prompt("Enter one word: ");
-    if (word === null) {
-        alert("Canceled.");
-    } else if (word.trim().includes(" ") || word.trim() === "") {
-        alert("Invalid data input.");
-    } else {
-        isInputValid = true;
-        word = word.trim();
-        if (word.length % 2 === 0) {
-            alert(`Word: ${word}, relust: ${word[word.length / 2 - 1]}${word[word.length / 2]}`)
-        } else {
-            alert(`Word: ${word}, relust: ${word[Math.floor(word.length / 2)]}`)
-        }
-    }
-
-}
+// if (word === null) {
+//   alert("Canceled.");
+// } else if (word.trim().includes(" ") || word.trim() === "") {
+//   alert("Invalid data input.");
+// } else {
+//   word = word.trim();
+//   if (word.length % 2 === 0) {
+//     alert(
+//       `Word: ${word}, result: ${word[word.length / 2 - 1]}${
+//         word[word.length / 2]
+//       }`
+//     );
+//   } else {
+//     alert(`Word: ${word}, result: ${word[Math.floor(word.length / 2)]}`);
+//   }
+// }
